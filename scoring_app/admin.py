0b6dc362b5d30ded46c38020a2fa50c5a64a5d48
QwantisheKU <odargov@gmail.com>
2023-02-26 18:02:38 +0300

    Created first version of website appearance and modified models

--- scoring_app/admin.py
+++ scoring_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Person
+from .models import User, Form
 
-admin.site.register(Person)
+admin.site.register(User)
+admin.site.register(Form)
--- scoring_app/models.py
+++ scoring_app/models.py
@@ -1,6 +1,25 @@
 from django.db import models
 
-class Person(models.Model):
+class User(models.Model):
     nick_name = models.CharField(max_length=30)
-    full_name = models.CharField(max_length=250)
     email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
+
+class Form(models.Model):
+    person_name = models.CharField(max_length=200)
+    person_age = models.IntegerField()
+    person_income = models.IntegerField()
+    person_home_ownership = models.CharField(max_length=30)
+    person_emp_length = models.FloatField()
+    loan_intent = models.CharField(max_length=30)
+    loan_amnt = models.IntegerField()
+    loan_int_rate = models.FloatField()
+    loan_percent_income = models.FloatField()
+    cb_person_default_on_file = models.FloatField()
+    cb_person_cred_hist_length = models.IntegerField()
+    person_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.person_id
