837cde01b78696fd91508759a5ae54ee94097a83
QwantisheKU <odargov@gmail.com>
2023-03-07 22:55:12 +0300

    Full authorization + showing calculations by user

--- scoring_app/forms.py
+++ scoring_app/forms.py
@@ -1,6 +1,52 @@
 from django.forms import ModelForm
 from .models import Calculation, CalculationResult
 from crispy_forms.helper import FormHelper
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+class SignInForm(AuthenticationForm):
+    email = forms.EmailField(
+        max_length=100,
+        required = True,
+        help_text='Введите email адрес',
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
+    )
+    password = forms.CharField(
+        help_text='Введите пароль',
+        required = True,
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '********'}),
+    )
+    class Meta:
+        model = User
+        fields = ['email', 'password']
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(
+        max_length=100,
+        required = True,
+        help_text='Введите email адрес',
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
+    )
+    password1 = forms.CharField(
+        help_text='Введите пароль',
+        required = True,
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '********'}),
+    )
+    password2 = forms.CharField(
+        required = True,
+        help_text='Повторите пароль',
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '********'}),
+    )
+    class Meta:
+        model = User
+        fields = ['email', 'password1', 'password2']
+    
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_show_labels = False
 
 class CalculationForm(ModelForm):
     class Meta:
@@ -14,7 +60,8 @@ class CalculationForm(ModelForm):
     'loan_amnt',
     'loan_int_rate',
     'cb_person_default_on_file',
-    'cb_person_cred_hist_length']
+    'cb_person_cred_hist_length',
+    ]
         
     def __init__(self, *args, **kwargs):
         super(CalculationForm, self).__init__(*args, **kwargs)
@@ -25,3 +72,4 @@ class CalculationResultForm(ModelForm):
     class Meta:
         model = CalculationResult
         fields = ['score', 'calculation_id']
+
--- scoring_app/views.py
+++ scoring_app/views.py
@@ -1,45 +1,67 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, QueryDict
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 from .models import Calculation, CalculationResult
-from .forms import CalculationForm, CalculationResultForm
+from .forms import CalculationForm, CalculationResultForm, SignUpForm, SignInForm
+from django.contrib.auth.decorators import login_required
 import pickle
 import script
+import time
 
 def sign_up(request):
-    return render(request, 'scoring_app/sign_up.html')
+    sign_up_form = SignUpForm()
+    context = {'sign_up_form': sign_up_form}
+    if request.method == 'POST':
+        sign_up_form = SignUpForm(request.POST)
+        if sign_up_form.is_valid():
+            sign_up_form.save()
+            return redirect('../sign-in/')
+    return render(request, 'scoring_app/sign_up.html', context)
 
 def sign_in(request):
-    return render(request, 'scoring_app/sign_in.html')
+    #sign_in_form = SignInForm()
+    #context = {'sign_in_form': sign_in_form}
+    context = {}
+    if request.method == 'POST':
+        #sign_in_form = SignInForm(request.POST)
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('../home/')
+    return render(request, 'scoring_app/sign_in.html', context)
 
+@login_required(login_url='sign-in')
+def sign_out(request):
+    logout(request)
+    return redirect('../sign-in/')
+
+@login_required(login_url='sign-in')
 def home(request):
-    calculations = Calculation.objects.all()
+    calculations = Calculation.objects.all().filter(user=request.user)
     context = {'calculations': calculations}
     return render(request, 'scoring_app/home.html', context)
 
+@login_required(login_url='sign-in')
 def form(request):
     calculation_form = CalculationForm()
-    calculation_result_form = CalculationResultForm()
     context = {'form': calculation_form}
-    score = []
     if request.method == 'POST':
         calculation_form = CalculationForm(request.POST)
         data = [int(request.POST.get('person_age')), int(request.POST.get('person_income')), request.POST.get('person_home_ownership'), int(request.POST.get('person_emp_length')), request.POST.get('loan_intent'), int(request.POST.get('loan_amnt')), float(request.POST.get('loan_int_rate')), request.POST.get('cb_person_default_on_file'), int(request.POST.get('cb_person_cred_hist_length'))]
         if calculation_form.is_valid():
+            saved_calculation_form = calculation_form.save(commit=False)
+            saved_calculation_form.user = request.user
             saved_calculation_form = calculation_form.save()
-            request.session['calculation_id'] = saved_calculation_form.pk
             request.session['data'] = data
-            score = script.preprocess_data(data)[0]
-            score_dict = {'score': score, 'calculation_id': saved_calculation_form.pk}
-            calculation_result_form_query_dict = QueryDict('', mutable=True)
-            calculation_result_form_query_dict.update(score_dict)
-            calculation_result_form = CalculationResultForm(calculation_result_form_query_dict)
-            if calculation_result_form.is_valid():
-                calculation_result_form.save()
-            return redirect('../result/')
+            return redirect(f'../result/?calculation_id={saved_calculation_form.pk}')
     return render(request, 'scoring_app/form.html', context)
 
+@login_required(login_url='sign-in')
 def calculation_result(request):
-    calculation_id = request.session.get('calculation_id')
+    calculation_id = request.GET.get('calculation_id')
     data = request.session.get('data')
     calculation_result_form = CalculationResultForm()
     score = script.preprocess_data(data)[0]
@@ -49,8 +71,11 @@ def calculation_result(request):
     calculation_result_form_query_dict.update(score_dict)
     calculation_result_form = CalculationResultForm(calculation_result_form_query_dict)
     if calculation_result_form.is_valid():
-        calculation_result_form.save()
+        saved_calculation_result_form = calculation_result_form.save(commit=False)
+        saved_calculation_result_form.user = request.user
+        saved_calculation_result_form.save()
     return render(request, 'scoring_app/result.html', context)
 
+@login_required(login_url='sign-in')
 def contact(request):
     return render(request, 'scoring_app/contact.html')
